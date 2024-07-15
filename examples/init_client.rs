313b17f074c6ca5a67ea24089f2437ec959b1cc9
Aleksandr <provotorov@inbox.ru>
2024-07-15 23:26:55 +0300

    category parameters

--- examples/init_client.rs
+++ examples/init_client.rs
@@ -1,24 +1,31 @@
 use anyhow::Result;
 use rust_yandexmarket::MarketClient;
+use rust_yandexmarket::models::CategoryDto;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let token = std::env::var("MARKET_TOKEN").expect("MARKET_TOKEN must be set");
     let client = MarketClient::new(token)?;
-    dbg!(client);
+    let all_categories = client.get_categories().await?;
+    let carpet_category = all_categories
+        .into_iter()
+        .flat_map(|c| search_in_categories_by_name(c, "ковролин"))
+        .collect::<Vec<_>>();
+    let carpet_category_parameters = client.get_category_parameters(carpet_category[0].id).await?;
+    dbg!(&carpet_category_parameters);
     Ok(())
 }
-// fn search_in_category(category: CategoryDto, search_string: &str) -> Option<CategoryDto> {
-//     if category.name.to_lowercase().contains(&search_string.to_lowercase()) {
-//         return Some(category)
-//     } else {
-//         if let Some(children) = category.children {
-//             for category in children {
-//                 if let Some(category) = search_in_category(category, search_string) {
-//                     return Some(category)
-//                 }
-//             }
-//         }
-//     }
-//     None
-// }
+fn search_in_categories_by_name(category: CategoryDto, search_string: &str) -> Option<CategoryDto> {
+    if category.name.to_lowercase().contains(&search_string.to_lowercase()) {
+        return Some(category)
+    } else {
+        if let Some(children) = category.children {
+            for category in children {
+                if let Some(category) = search_in_categories_by_name(category, search_string) {
+                    return Some(category)
+                }
+            }
+        }
+    }
+    None
+}
--- src/lib.rs
+++ src/lib.rs
@@ -4,16 +4,49 @@
 //! Библиотека для работы с API Yandex.Market на языке программирования Rust
 
 use anyhow::Result;
-mod models;
+use reqwest::Url;
+
+use crate::models::{CategoryDto, CategoryParameterDto, GetCategoriesResponse, GetCategoryContentParametersResponse};
+
+pub mod models;
+
 #[derive(Debug)]
 pub struct MarketClient {
     token: String,
+    base_url: Url,
     client: reqwest::Client,
 }
 impl MarketClient {
     pub fn new<T: ToString>(token: T) -> Result<Self> {
         let token = token.to_string();
+        let base_url = Url::parse("https://api.partner.market.yandex.ru")?;
         let client = reqwest::Client::builder().gzip(true).build()?;
-        Ok(Self { token, client })
+        Ok(Self { token, base_url, client })
+    }
+    pub async fn get_categories(&self) -> Result<Vec<CategoryDto>> {
+        let uri = self.base_url.join("categories/tree")?;
+        let result: GetCategoriesResponse = self
+            .client
+            .post(uri)
+            .bearer_auth(&self.token)
+            .send()
+            .await?
+            .json()
+            .await?;
+        Ok(result.result.and_then(|r| r.children).unwrap_or_default())
+    }
+    pub async fn get_category_parameters(&self, category_id: i64) -> Result<Vec<CategoryParameterDto>> {
+        let endpoint = format!("category/{category_id}/parameters");
+        let uri = self.base_url.join(&endpoint)?;
+        let response: GetCategoryContentParametersResponse = self
+            .client
+            .post(uri)
+            .bearer_auth(&self.token)
+            .send()
+            .await?
+            .json()
+            .await?;
+        let result = response.result.and_then(|r| r.parameters).unwrap_or_default();
+        Ok(result)
     }
 }
